a96e69e4712ee0a4ab8c3b16ce1393340278029d
Mathis <90392683+mmaatthhiiss@users.noreply.github.com>
2021-12-09 15:56:40 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+		<title>Mon CV</title>
+		<link rel="stylesheet" href="style.css">
+		<script src="script.js"></script>
+
+</head>
+
+<body>
+	<header>
+		<?php include("php/menu.php");?>
+	</header>
+
+	<main>
+			
+
+				<?php include("php/presentation.php");?>
+				<?php include("php/propo.php"); ?>
+				<?php include("php/competence.php"); ?>
+				<?php include("php/experience.php");?>
+				<?php include("php/foramtion.php"); ?>
+				<?php include("php/contact.php"); ?>
+			
+		</main>
+	
+</body>
+
+</html>
